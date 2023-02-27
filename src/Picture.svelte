d8de1d1f836003ed7d993d721e3c0056ef361870
James Freeman <james@jammaloo.com>
2023-02-27 00:37:39 -0500

    feat: add basic picture component

---
+++ src/Picture.svelte
@@ -0,0 +1,38 @@
+<script>
+
+</script>
+
+<div class="outer-frame">
+    <div class="inner-frame">
+        <div class="inner-inner-frame">
+            <div class="picture">
+                <slot />
+            </div>
+        </div>
+    </div>
+</div>
+
+<style>
+.outer-frame {
+    border: 1em solid var(--dark);
+    background: var(--dark);
+}
+
+.inner-frame {
+    border: 0.25em dashed var(--light);
+    background: var(--bg);
+}
+
+.inner-inner-frame {
+    border: 1em solid var(--dark);
+    background: var(--light);
+}
+
+.picture {
+    border: 2em solid var(--light);
+    background: var(--bg);
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,8 @@
+<script>
+import Picture from '../Picture.svelte';
+
+</script>
 <h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<Picture>
+    <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+</Picture>
