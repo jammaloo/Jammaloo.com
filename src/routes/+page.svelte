c241623d5dfbf1827013e6cf79506cbfdfbeee2d
James Freeman <james@jammaloo.com>
2023-03-07 09:04:30 -0500

    feat: add more frames

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,12 +1,46 @@
 <script>
 import Picture from '../Picture.svelte';
-let headshot = 'headshot.jpg';
+const headshot = 'headshot.jpg';
+const eyeball = 'eyeball.png';
 
 </script>
-<h1>James Freeman</h1>
-<Picture>
-    <img src={headshot} alt="James Freeman headshot" />
-</Picture>
-<Picture>
-    <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
-</Picture>
+
+<div class="pictures">
+    <Picture>
+        <img src={headshot} alt="James Freeman headshot" />
+    </Picture>
+
+    <Picture>
+        <p>
+            <a href="https://blog.apollo350.com/posts/easy-ci-cd-with-github-actions">Easy CI/CD with Github Actions</a>
+        </p>
+    </Picture>
+
+    <Picture>
+        <h1>James Freeman</h1>
+    </Picture>
+
+    <Picture>
+        <p>
+            <a href="https://blog.apollo350.com/posts/video-calls-in-the-time-of-covid">Video Calls in the Time of Covid</a>
+        </p>
+    </Picture>
+
+    <Picture>
+        <iframe src="https://jammaloo.com/faces/"></iframe>
+    </Picture>
+
+    <Picture>
+        <a href="https://jammaloo.com/random/EyeSeeYou/dist/">
+            <img src={eyeball} alt="A colourful eyeball" />
+        </a>
+    </Picture>
+</div>
+
+<style>
+    iframe {
+        border: none;
+    }
+    .pictures {
+    }
+</style>
